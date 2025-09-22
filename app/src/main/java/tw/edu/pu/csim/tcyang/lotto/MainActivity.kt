649865693e8951c6c68a7658fd2077b0bff67c08
a0909123137 <a0909123137@gmail.com>
2025-09-22 15:21:55 +0800

    第七次commit (螢幕觸控)(林煥杰)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -1,9 +1,11 @@
 package tw.edu.pu.csim.tcyang.lotto
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,10 +19,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.lotto.ui.theme.LottoTheme
 
 import androidx.compose.runtime.setValue // 引入 setValue
+import androidx.compose.ui.platform.LocalContext
 
 
 class MainActivity : ComponentActivity() {
@@ -45,11 +47,16 @@ fun Play(modifier: Modifier = Modifier) {
     var lucky by remember {
         mutableStateOf((1..100).random())
     }
+    
+    val context = LocalContext.current
 
-    Column (modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-        ){
+     Column(modifier = modifier
+         .fillMaxSize()
+         .clickable{
+             Toast.makeText(context,"螢幕觸控(林煥杰)", Toast.LENGTH_LONG).show()
+         } ,
+             horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement . Center){
         Text(
             text = "樂透數字(1-100)為 $lucky"
         )
